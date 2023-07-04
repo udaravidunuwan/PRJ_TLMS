a2aada810efb190aec9066dd4d20f982610fbf01
udaravidunuwan <udara.v.nuwan.0@gmail.com>
2023-07-05 00:14:32 +0530

    color choosing

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,12 +28,3 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-// Settings Dropdown functionality
-function toggleDropdown() {
-    var dropdown = document.getElementById("dropdown__settings");
-    if (dropdown.style.display === "none") {
-      dropdown.style.display = "block";
-    } else {
-      dropdown.style.display = "none";
-    }
-  }
